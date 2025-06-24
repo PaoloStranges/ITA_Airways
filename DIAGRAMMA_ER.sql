8355b33dc91485a9e8cc510607c82a84b5c59251
PaoloStranges <stranges_ho@hotmail.it>
2025-06-24 23:58:14 +0200

    Update DIAGRAMMA_ER.sql

--- DIAGRAMMA_ER.sql
+++ DIAGRAMMA_ER.sql
@@ -1,89 +1,57 @@
-CREATE TABLE "Cliente" (
-  "IDCliente" SERIAL PRIMARY KEY,
-  "Nome" "VARCHAR(50)" NOT NULL,
-  "Cognome" "VARCHAR(50)" NOT NULL,
-  "Email" "VARCHAR(100)" UNIQUE NOT NULL,
-  "Telefono" "VARCHAR(20)",
-  "DataNascita" "DATE",
-  "IscrizioneVolare" "BOOLEAN" DEFAULT false
-);
 
-CREATE TABLE "Aeroporto" (
-  "IDAeroporto" SERIAL PRIMARY KEY,
-  "Nome" "VARCHAR(100)",
-  "CodiceIATA" "CHAR (3)" UNIQUE,
-  "CodiceICAO" "CHAR (4)" UNIQUE,
-  "Città" "VARCHAR(100)",
-  "Nazione" "VARCHAR(100)"
-);
+Table "Aereo" {
+  "id_aereo" INT [pk]
+  "modello" VARCHAR(50)
+  "capacità" INT
+  "consumo_medio" DECIMAL(5,2)
+}
 
-CREATE TABLE "Aeromobile" (
-  "IDAeromobile" SERIAL PRIMARY KEY,
-  "Modello" "VARCHAR(50)",
-  "AnnoImmatricolazione" "INT",
-  "Capienza" "INT",
-  "Consumo" "DECIMAL(6,2)",
-  "AutonomiaKM" "INT"
-);
+Table "Tratta" {
+  "id_tratta" INT [pk]
+  "aeroporto_partenza" VARCHAR(10)
+  "aeroporto_arrivo" VARCHAR(10)
+  "distanza" INT
+  "durata_stimata" INT
+}
 
-CREATE TABLE "Dipendente" (
-  "IDDipendente" SERIAL PRIMARY KEY,
-  "Nome" "VARCHAR(50)",
-  "Cognome" "VARCHAR(50)",
-  "Ruolo" "VARCHAR(50)",
-  "Certificazioni" "TEXT",
-  "OreDiVolo" "INT"
-);
+Table "Viaggio" {
+  "id_viaggio" INT [pk]
+  "id_aereo" INT
+  "id_tratta" INT
+  "data_partenza" DATE
+  "ora_partenza" TIME
+}
 
-CREATE TABLE "Volo" (
-  "IDVolo" SERIAL PRIMARY KEY,
-  "DataPartenza" "DATE",
-  "OraPartenza" "TIME",
-  "DataArrivo" "DATE",
-  "OraArrivo" "TIME",
-  "Stato" "VARCHAR(30)",
-  "IDAeroportoPartenza" "INT",
-  "IDAeroportoArrivo" "INT",
-  "IDAeromobile" "INT",
-  "PostiDisponibili" "INT",
-  "DurataStimata" "INTERVAL"
-);
+Table "PosizioneAereo" {
+  "id_posizione" INT [pk]
+  "id_aereo" INT
+  "latitudine" DECIMAL(9,6)
+  "longitudine" DECIMAL(9,6)
+  "altitudine" INT
+  "timestamp" TIMESTAMP
+}
 
-CREATE TABLE "Prenotazione" (
-  "IDPrenotazione" SERIAL PRIMARY KEY,
-  "DataPrenotazione" "TIMESTAMP" DEFAULT (CURRENT_TIMESTAMP),
-  "Stato" "VARCHAR(30)",
-  "MetodoPagamento" "VARCHAR(50)",
-  "IDCliente" "INT",
-  "CodiceSconto" "VARCHAR(20)"
-);
+Table "EventoOperativo" {
+  "id_evento" INT [pk]
+  "id_aereo" INT
+  "tipo_evento" VARCHAR(50)
+  "descrizione" TEXT
+  "data_ora" TIMESTAMP
+}
 
-CREATE TABLE "Prenotazione_Volo" (
-  "IDPrenotazione" "INT",
-  "IDVolo" "INT",
-  "PostoAssegnato" "VARCHAR(5)",
-  PRIMARY KEY ("IDPrenotazione", "IDVolo")
-);
+Table "ConsumoCarburante" {
+  "id_consumo" INT [pk]
+  "id_viaggio" INT
+  "litri_consumati" DECIMAL(6,2)
+  "durata_effettiva" INT
+}
 
-CREATE TABLE "Equipaggio" (
-  "IDVolo" "INT",
-  "IDDipendente" "INT",
-  "RuoloInVolo" "VARCHAR(50)",
-  PRIMARY KEY ("IDVolo", "IDDipendente")
-);
+Ref:"Aereo"."id_aereo" < "Viaggio"."id_aereo"
 
-ALTER TABLE "Volo" ADD FOREIGN KEY ("IDAeroportoPartenza") REFERENCES "Aeroporto" ("IDAeroporto");
+Ref:"Tratta"."id_tratta" < "Viaggio"."id_tratta"
 
-ALTER TABLE "Volo" ADD FOREIGN KEY ("IDAeroportoArrivo") REFERENCES "Aeroporto" ("IDAeroporto");
+Ref:"Aereo"."id_aereo" < "PosizioneAereo"."id_aereo"
 
-ALTER TABLE "Volo" ADD FOREIGN KEY ("IDAeromobile") REFERENCES "Aeromobile" ("IDAeromobile");
+Ref:"Aereo"."id_aereo" < "EventoOperativo"."id_aereo"
 
-ALTER TABLE "Prenotazione" ADD FOREIGN KEY ("IDCliente") REFERENCES "Cliente" ("IDCliente");
-
-ALTER TABLE "Prenotazione_Volo" ADD FOREIGN KEY ("IDPrenotazione") REFERENCES "Prenotazione" ("IDPrenotazione") ON DELETE CASCADE;
-
-ALTER TABLE "Prenotazione_Volo" ADD FOREIGN KEY ("IDVolo") REFERENCES "Volo" ("IDVolo") ON DELETE CASCADE;
-
-ALTER TABLE "Equipaggio" ADD FOREIGN KEY ("IDVolo") REFERENCES "Volo" ("IDVolo") ON DELETE CASCADE;
-
-ALTER TABLE "Equipaggio" ADD FOREIGN KEY ("IDDipendente") REFERENCES "Dipendente" ("IDDipendente") ON DELETE CASCADE;
+Ref:"Viaggio"."id_viaggio" < "ConsumoCarburante"."id_viaggio"
