f930f45e823e5a5823c94509cc79cb75559df4d3
PaoloStranges <stranges_ho@hotmail.it>
2025-06-28 12:35:15 +0200

    Update ITA_Airways_DB_script.sql

--- ITA_Airways_DB_script.sql
+++ ITA_Airways_DB_script.sql
@@ -79,3 +79,42 @@ INSERT INTO ConsumoCarburante (id_viaggio, litri_consumati, durata_effettiva) VA
 (2, 500.25, 25),
 (3, 17250.25, 545),
 (4, 16980.75, 530);
+
+
+### All'interno di `ITA_Airways_DB_script.sql`, puoi aggiungere le query alla fine del file:
+
+```sql
+-- Query dimostrative
+
+-- 1. Ultima posizione GPS per aereo
+SELECT * FROM PosizioneAereo
+WHERE id_aereo = 1
+ORDER BY timestamp DESC
+LIMIT 1;
+
+-- 2. Storico eventi tecnici per aereo
+SELECT * FROM EventoOperativo
+WHERE id_aereo = 1
+ORDER BY data_ora DESC;
+
+-- 3. Consumo medio per modello di aereo
+SELECT Aereo.modello, AVG(ConsumoCarburante.litri_consumati) AS consumo_medio
+FROM Aereo
+JOIN Viaggio ON Aereo.id_aereo = Viaggio.id_aereo
+JOIN ConsumoCarburante ON Viaggio.id_viaggio = ConsumoCarburante.id_viaggio
+GROUP BY Aereo.modello;
+
+-- 4. Viaggi programmati tra due aeroporti (es. FCO → JFK)
+SELECT * FROM Viaggio
+JOIN Tratta ON Viaggio.id_tratta = Tratta.id_tratta
+WHERE Tratta.aeroporto_partenza = 'FCO' AND Tratta.aeroporto_arrivo = 'JFK'
+ORDER BY Viaggio.data_partenza;
+
+-- 5. Tratte con maggiore consumo totale in un intervallo di tempo
+SELECT Tratta.aeroporto_partenza, Tratta.aeroporto_arrivo, SUM(ConsumoCarburante.litri_consumati) AS consumo_totale
+FROM Tratta
+JOIN Viaggio ON Tratta.id_tratta = Viaggio.id_tratta
+JOIN ConsumoCarburante ON Viaggio.id_viaggio = ConsumoCarburante.id_viaggio
+WHERE Viaggio.data_partenza BETWEEN '2025-06-01' AND '2025-06-30'
+GROUP BY Tratta.aeroporto_partenza, Tratta.aeroporto_arrivo
+ORDER BY consumo_totale DESC;
