53a7ca98e1a14e640d346093d8d615112df6d763
PaoloStranges <stranges_ho@hotmail.it>
2025-07-08 00:56:53 +0200

    Update DIAGRAMMA_ER.sql

--- DIAGRAMMA_ER.sql
+++ DIAGRAMMA_ER.sql
@@ -1,54 +1,45 @@
-Table "Aereo" {
-  "id_aereo" INT [pk]
-  "modello" VARCHAR(50)
-  "capacita" INT
-  "consumo_medio" DECIMAL(5,2)
+Table Passeggero {
+  id_passeggero INT [pk]
+  nome VARCHAR(50)
+  cognome VARCHAR(50)
 }
 
-Table "Tratta" {
-  "id_tratta" INT [pk]
-  "aeroporto_partenza" VARCHAR(10)
-  "aeroporto_arrivo" VARCHAR(10)
-  "distanza" INT
-  "durata_stimata" INT
+Table Aereo {
+  id_aereo INT [pk]
+  modello VARCHAR(50)
+  capacita INT
 }
 
-Table "Viaggio" {
-  "id_viaggio" INT [pk]
-  "id_aereo" INT
-  "id_tratta" INT
-  "data_partenza" DATE
-  "ora_partenza" TIME
+Table Tratta {
+  id_tratta INT [pk]
+  aeroporto_partenza VARCHAR(10)
+  aeroporto_arrivo VARCHAR(10)
+  distanza_km INT
 }
 
-Table "PosizioneAereo" {
-  "id_posizione" INT [pk]
-  "id_aereo" INT
-  "latitudine" DECIMAL(9,6)
-  "longitudine" DECIMAL(9,6)
-  "altitudine" INT
-  "timestamp" TIMESTAMP
+Table Viaggio {
+  id_viaggio INT [pk]
+  id_aereo INT [ref: > Aereo.id_aereo]
+  data_partenza DATE
 }
 
-Table "EventoOperativo" {
-  "id_evento" INT [pk]
-  "id_aereo" INT
-  "id_viaggio" INT
-  "tipo_evento" VARCHAR(50)
-  "descrizione" TEXT
-  "data_ora" TIMESTAMP
+Table Scalo {
+  id_scalo INT [pk]
+  id_viaggio INT [ref: > Viaggio.id_viaggio]
+  id_tratta INT [ref: > Tratta.id_tratta]
+  ordine_scalo INT
 }
 
-Table "ConsumoCarburante" {
-  "id_consumo" INT [pk]
-  "id_viaggio" INT
-  "litri_consumati" DECIMAL(6,2)
-  "durata_effettiva" INT
+Table Biglietto {
+  id_biglietto INT [pk]
+  id_passeggero INT [ref: > Passeggero.id_passeggero]
+  id_viaggio INT [ref: > Viaggio.id_viaggio]
+  stato VARCHAR(20)
 }
 
-Ref: "Aereo"."id_aereo" < "Viaggio"."id_aereo"
-Ref: "Tratta"."id_tratta" < "Viaggio"."id_tratta"
-Ref: "Aereo"."id_aereo" < "PosizioneAereo"."id_aereo"
-Ref: "Aereo"."id_aereo" < "EventoOperativo"."id_aereo"
-Ref: "Viaggio"."id_viaggio" < "EventoOperativo"."id_viaggio"
-Ref: "Viaggio"."id_viaggio" < "ConsumoCarburante"."id_viaggio"
+Table Prenotazione {
+  id_prenotazione INT [pk]
+  id_biglietto INT [ref: > Biglietto.id_biglietto]
+  data_prenotazione DATE
+  stato VARCHAR(20)
+}
